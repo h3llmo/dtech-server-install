85b980c37b8c5c0acc608738d102796b00a1ae97
Pascual Sondag <damosoft.technologies@gmail.com>
2023-04-04 14:37:44 +0200

    Install Docker file

---
+++ Files/installDocker.sh
@@ -0,0 +1,5 @@
+sudo dpkg -i /media/dtech/Docker/containerd.io_1.6.9-1_amd64.deb \
+/media/dtech/Docker/docker-ce_23.0.2-1_ubuntu.22.04_jammy_amd64.deb \
+/media/dtech/Docker/docker-ce-cli_23.0.2-1_ubuntu.22.04_jammy_amd64.deb \
+/media/dtech/Docker/docker-buildx-plugin_0.10.4-1_ubuntu.22.04_jammy_amd64.deb \
+/media/dtech/Docker/docker-compose-plugin_2.17.2-1_ubuntu.22.04_jammy_amd64.deb
