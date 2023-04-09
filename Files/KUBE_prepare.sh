09a355ba5aa2ccf0c125fc04052217a2a320872c
h3llmo <h3llmo@gmail.com>
2023-04-09 08:05:35 +0200

    Update KUBE_prepare.sh

--- Files/KUBE_prepare.sh
+++ Files/KUBE_prepare.sh
@@ -3,6 +3,6 @@ sudo microk8s enable metrics-server
 sudo microk8s enable dashboard
 sudo usermod -a -G microk8s vagrant
 sudo mkdir -p /var/snap/microk8s/current/args/certs.d/$1:$2
-MY_DOMAIN=$1 envsubst < /media/dtech/Files/ContainerdToml/hosts.toml >> /var/snap/microk8s/current/args/certs.d/$1:$2/
+MY_DOMAIN=$1, CR_PORT=$2 envsubst < /media/dtech/Files/ContainerdToml/hosts.toml >> /var/snap/microk8s/current/args/certs.d/$1:$2/
 sudo microk8s stop
 sudo microk8s start
